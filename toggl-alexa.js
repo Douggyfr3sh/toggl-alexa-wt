cc092692625906fda561c5ec134e7c5240fb8d13
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-08-23 13:48:57 -0400

    Reverted to ES5 syntax as "use latest" was breaking my app.  Basic browser tests now passing.

--- toggl-alexa.js
+++ toggl-alexa.js
@@ -1,45 +1,59 @@
-use latest; //ES6
+//use latest; //ES6- Had problems with this.
 
 //Require statements for async code and mongodb interface
 /* Used guide at:
 
 https://auth0.com/blog/if-this-then-node-dot-js-extending-ifttt-with-webtask-dot-io/
 */
-var parallel = require('async').parallel;
+//var parallel = require('async').parallel;
 var MongoClient = require('mongodb').MongoClient;
+
+
+//This Fn stores a new task's data to MongoDB
+function save_task(doc, db, cb) {
+  //store new data to our mLab DB
+  db
+    .collection('Tasks')
+    .insertOne(doc, function (err, res) {
+      if (err) return cb(err);
+
+      console.log(`Successfully saved ${doc.description} task!`);
+
+      cb(null);
+    });
+}
 
 
 //Main entry function
 module.exports = function (ctx, done) {
-
   console.log(ctx);
 
   //Connect to MongoDB to store data
-  MongoClient.connect(ctx.dada.MONGO_URL, (err,db) => {
+  MongoClient.connect(ctx.data.MONGO_URL, (err,db) => {
 
     if (err) return done(err);
 
-    //define the data to be saved
-    let doc = {
-      project: ctx.data.project;,
-      duration: ctx.data.duration,
-      description: ctx.data.description,
+    //define the data to be saved in a document
+    var doc = {
+      project: ctx.data.project || 'None',
+      duration: ctx.data.duration || 0,
+      description: ctx.data.description || 'N/A',
       timestamp: Date.now()
     }
 
-    //store new data to our mLab DB
-    db
-      .collection('Tasks')
-      .insertOne(doc, (err, res) => {
-        if (err) return err;
+    save_task(doc,db, function (err) {
+      if (err) return done(err);
 
-        console.log('Successfully saved ${doc.description} task!');
+      done(null, 'Success.');
+    });
 
-        done(null, 'Success');
-      });
+    // parallel(save_task, (err) => {
+    //   if (err) return done(err);
 
+    //   done(null, 'Success.');
+    // });
 
   });
 
-  //done(null, 'Hello, ' + ctx.data.name);
+  //done(null, 'It\'s Working!');
 }
